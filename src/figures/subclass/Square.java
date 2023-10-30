84c4563db12001479355e3cbefe8f601ad25928d
tineojf <92743762+tineodev@users.noreply.github.com>
2023-10-30 13:30:52 -0500

    Add attribute in Square class

--- src/figures/subclass/Square.java
+++ src/figures/subclass/Square.java
@@ -1,14 +1,17 @@
 package figures.subclass;
 
 public class Square extends figures.subclass.Parallelogram {
+    private final double side;
+
     // Constructor
     public Square(double side) {
         super("Square", side, side);
+        this.side = side;
     }
 
     // To String
     public String toString() {
         return super.toString()
-                + "Side: " + super.getBase();
+                + "Side: " + this.side;
     }
 }
