3eda4e435100ab7943dd5b65ca00fd3f1adad32a
tineojf <92743762+tineodev@users.noreply.github.com>
2023-10-30 18:43:14 -0500

    Add Ahorro class & example in Main class

--- src/cuentas/Main.java
+++ src/cuentas/Main.java
@@ -1,4 +1,14 @@
 package cuentas;
 
+import cuentas.subclass.Ahorro;
+
 public class Main {
+    public static void main(String[] args) {
+        Ahorro cuentaAhorro = new Ahorro(Cuenta.TypeAccount.AHORRO, "Juan", "Perez", "123456789", 0.1);
+        System.out.println(cuentaAhorro);
+        cuentaAhorro.depositar(100);
+        System.out.println();
+        cuentaAhorro.aplicarInteres();
+        System.out.println("--------------------");
+    }
 }
--- src/cuentas/subclass/Ahorro.java
+++ src/cuentas/subclass/Ahorro.java
@@ -1,4 +1,25 @@
 package cuentas.subclass;
 
-public class Ahorro {
+public class Ahorro extends cuentas.Cuenta {
+    private final double interes;
+
+    // Constructor
+    public Ahorro(TypeAccount typeAccount, String nombres, String apellidos, String id, double interes) {
+        super(TypeAccount.AHORRO, nombres, apellidos, id);
+        this.interes = interes;
+    }
+
+    // To String
+    @Override
+    public String toString() {
+        return super.toString() +
+                "Interes: " + interes + "\n";
+    }
+
+    // Methods
+    public void aplicarInteres() {
+        double interes = this.getSaldo() * this.interes;
+        this.depositar(interes);
+    }
+
 }
