854800bfee763c4aa0aab814b6f078abcde9d40c
tineojf <92743762+tineodev@users.noreply.github.com>
2023-10-30 14:13:14 -0500

    Add Triangle

--- src/figures/Main.java
+++ src/figures/Main.java
@@ -3,6 +3,7 @@ package figures;
 import figures.subclass.Circle;
 import figures.subclass.Rectangle;
 import figures.subclass.Square;
+import figures.subclass.Triangle;
 
 public class Main {
     public static void main(String[] args) {
@@ -17,5 +18,18 @@ public class Main {
         Rectangle rectangle = new Rectangle(10, 20);
         System.out.println(rectangle);
         System.out.println("--------------------");
+
+        Triangle triangle = new Triangle(10, 10, 10);
+        System.out.println(triangle);
+        System.out.println("--------------------");
+
+        Triangle triangle2 = new Triangle(7, 7, 5);
+        System.out.println(triangle2);
+        System.out.println("--------------------");
+
+        Triangle triangle3 = new Triangle(5, 12, 13);
+        System.out.println(triangle3);
+        System.out.println("--------------------");
+
     }
 }
--- src/figures/subclass/Triangle.java
+++ src/figures/subclass/Triangle.java
@@ -1,4 +1,56 @@
 package figures.subclass;
 
-public class Triangle {
+public class Triangle extends figures.Figure {
+    private enum TriangleType {EQUILATERAL, ISOSCELES, SCALENE}
+
+    private final double side1;
+    private final double side2;
+    private final double side3;
+    private final TriangleType type;
+    private final double area;
+
+    // Constructor
+    public Triangle(double side1, double side2, double side3) {
+        super("Triangle");
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+        this.type = this.calculateTriangleType();
+        this.area = this.calculateArea();
+    }
+
+    // Getters
+    public double getArea() {
+        return this.area;
+    }
+
+    // To String
+    public String toString() {
+        return super.toString()
+                + "Side 1: " + this.side1 + "\n"
+                + "Side 2: " + this.side2 + "\n"
+                + "Side 3: " + this.side3 + "\n"
+                + "Type: " + this.type + "\n"
+                + "Area: " + this.area + " u^2" + "\n";
+    }
+
+
+    // Methods
+    private TriangleType calculateTriangleType() {
+        if (this.side1 == this.side2 && this.side2 == this.side3) {
+            return TriangleType.EQUILATERAL;
+        } else if (this.side1 == this.side2 || this.side1 == this.side3 || this.side2 == this.side3) {
+            return TriangleType.ISOSCELES;
+        } else {
+            return TriangleType.SCALENE;
+        }
+    }
+
+    public double calculateArea() {
+        double p = (this.side1 + this.side2 + this.side3) / 2;
+        double area = Math.sqrt(p * (p - this.side1) * (p - this.side2) * (p - this.side3));
+        area = Math.round(area * 100.0) / 100.0;
+        return area;
+    }
+
 }
